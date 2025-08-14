e8b4f77ed498b9f32fbce786c3ad5de50632e5e9
Rodrigo Miranda da Silva <119507990+Rodrimir@users.noreply.github.com>
2025-08-14 16:37:11 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # clone-tabnews
+
 git add
 Implementação do https://www.tabnews.com.br para aulas do https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Fala cabeça opaa </h1>
+  return <h1> Fala cabeça opaa </h1>;
 }
 
 export default Home;
