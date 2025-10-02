1175a753a5f7ed65219928ca5f1ebc356016a663
Rodrigo Miranda da Silva <119507990+Rodrimir@users.noreply.github.com>
2025-10-02 17:03:13 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ function Home({ posts }) {
       <h1 style={{ marginBottom: '2rem', fontSize: '2.5rem' }}>Página Inicial</h1>
 
       <div style={{ marginBottom: '2.5rem' }}>
-        <Link href="/plano">
-          <a style={{
+        <Link href="/plano" style={{
             display: 'inline-block',
             padding: '1rem 2rem',
             fontSize: '1.1rem',
@@ -19,7 +18,6 @@ function Home({ posts }) {
             boxShadow: '0 4px 14px 0 rgba(0, 118, 255, 0.39)'
           }}>
             Acessar Meu Plano Semanal
-          </a>
         </Link>
       </div>
 
@@ -27,8 +25,8 @@ function Home({ posts }) {
       <ul style={{ listStyle: 'none', padding: 0 }}>
         {posts.map((post) => (
           <li key={post.id} style={{ marginBottom: '1rem' }}>
-            <Link href={`/${post.owner_username}/${post.slug}`}>
-              <a style={{ fontSize: '1.2rem', textDecoration: 'none', color: '#333' }}>{post.title}</a>
+            <Link href={`/${post.owner_username}/${post.slug}`} style={{ fontSize: '1.2rem', textDecoration: 'none', color: '#333' }}>
+              {post.title}
             </Link>
           </li>
         ))}
@@ -40,6 +38,7 @@ function Home({ posts }) {
 export async function getStaticProps() {
   const res = await fetch("https://www.tabnews.com.br/api/v1/contents");
   const posts = await res.json();
+
   return {
     props: {
       posts: posts.slice(0, 15),
@@ -49,3 +48,4 @@ export async function getStaticProps() {
 }
 
 export default Home;
+
